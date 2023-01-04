b7d729e9c2c12359bb5f997c709ecf98854a65b7
Prateek Gandhi <prateek.gandhi@lntinfotech.com>
2023-01-04 17:21:21 +0530

    home.js

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="flex flex-col py-10 px-20 border-solid border-2 border-black">
+      <div className='text-base' style={{color: '#323130'}}>DAILY STACKS</div>
+      <div>1</div>
+      <div>1</div>
+    </div>
+  )
+}
+
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sanspro': ['Source Sans Pro', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
