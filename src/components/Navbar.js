94ef8bfa12282d0c30018583a3239ff41f50e41e
Prateek Gandhi <prateek.gandhi@lntinfotech.com>
2023-01-04 13:46:39 +0530

    remove borders and padding in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,20 +5,14 @@ import {MdOutlineKeyboardArrowDown} from "react-icons/md"
 
 const Navbar = () => {
   return (
-    <div className="grid grid-cols-2 py-10 px-20 border-black border-2">
-      <div class="p-1 border-cyan-900 border-2">Logo of TheTox</div>
-      <div class="grid grid-cols-4 md:grid-cols-8 justify-items-end  border-cyan-900 border-2">
-        <div class="md:col-start-5 p-2 border-cyan-900 border-2"><AiOutlinePlus/></div>
-        <div class="p-2 border-cyan-900 border-2"><BsBell/></div>
-        <div class="p-2 rounded-full ring-2 ring-fuchsia-600 ">Pict</div>
-        <div class="p-2 border-cyan-900 border-2"><MdOutlineKeyboardArrowDown/></div>
+    <div className="grid grid-cols-2 py-10 px-20">
+      <div>Logo of TheTox</div>
+      <div className="grid grid-cols-4 md:grid-cols-8 justify-items-end">
+        <div className="md:col-start-5 "><AiOutlinePlus/></div>
+        <div><BsBell/></div>
+        <div className="rounded-full ring-2 ring-fuchsia-600 ">Pict</div>
+        <div><MdOutlineKeyboardArrowDown/></div>
       </div>
-      {/* <div class="grid grid-cols-8 justify-items-end  border-cyan-900 border-2">
-        <div class="col-start-5 p-2 border-cyan-900 border-2"><AiOutlinePlus/></div>
-        <div class="p-2 border-cyan-900 border-2"><BsBell/></div>
-        <div class="p-2 rounded-full ring-2 ring-fuchsia-600 ">Pict</div>
-        <div class="p-2 border-cyan-900 border-2"><MdOutlineKeyboardArrowDown/></div>
-      </div> */}
     </div>
   );
 };
