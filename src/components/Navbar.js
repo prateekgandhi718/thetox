fcab7fe0f9432eb5036411393ee808fd40f64e08
Prateek Gandhi <prateek.gandhi@lntinfotech.com>
2023-01-05 17:52:15 +0530

    responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,30 @@
 import React from "react";
-import {AiOutlinePlus} from "react-icons/ai"
-import {BsBell} from "react-icons/bs"
-import {MdOutlineKeyboardArrowDown} from "react-icons/md"
+import { AiOutlinePlus } from "react-icons/ai";
+import { BsBell } from "react-icons/bs";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="grid grid-cols-2 py-10 px-20">
-      <Link to = "/">Logo of TheTox</Link>
+      <Link to="/" className="flex items-center">Logo of TheTox</Link>
       <div className="grid grid-cols-4 md:grid-cols-8 justify-items-end">
-        <div className="md:col-start-5 "><AiOutlinePlus/></div>
-        <Link to="/notification"><BsBell/></Link>
-        <Link to="/user" className="rounded-full ring-2 ring-fuchsia-600 ">Pict</Link>
-        <div><MdOutlineKeyboardArrowDown/></div>
+        <div className="flex items-center justify-center md:col-start-5 ">
+          <AiOutlinePlus />
+        </div>
+        <Link to="/notification" className="flex items-center justify-center">
+          <BsBell />
+        </Link>
+        <Link to="/user" className="flex items-center justify-center">
+          <img
+            class="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
+            src={require("../Designs/ProfilePic.jpg")}
+            alt="Bordered avatar"
+          />
+        </Link>
+        <div className="flex items-center justify-center">
+          <MdOutlineKeyboardArrowDown />
+        </div>
       </div>
     </div>
   );
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -2,72 +2,105 @@ import React from "react";
 
 const Summary = () => {
   return (
-    <div className="flex flex-col py-10 px-20 gap-8 border-solid border-2 border-black">
-      <div className="flex flex-col items-center bg-transparent md:flex-row md:gap-10 border-solid border-2 border-black">
-        <img
-          className="object-cover w-full rounded-full h-30 md:h-20 md:w-20 md:rounded-full"
-          src={require("../Designs/TwitterImage.jpeg")}
-          alt=""
-        />
-        <div className="flex flex-col justify-between p-4 leading-normal">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Twitter summary
-          </h5>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            For Prateek Gandhi
-          </p>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            11 curated tweets
-          </p>
+    <>
+      <div className="flex flex-col py-10 px-20 gap-8 border-solid ">
+        <div className="flex flex-col items-center bg-transparent md:flex-row md:gap-10 border-solid ">
+          <img
+            className="object-cover mb-1 rounded-full h-20 w-20 md:h-20 md:w-20 md:rounded-full"
+            src={require("../Designs/TwitterImageLogo.png")}
+            alt=""
+          />
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Twitter summary
+            </h5>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              For Prateek Gandhi
+            </p>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              11 curated tweets
+            </p>
+          </div>
         </div>
       </div>
 
-      <div className="grid grid-cols-6 gap-8 border-solid border-2 border-black">
-        <div className="border-solid hidden md:block border-2 border-black">the bars</div>
-        <div className="col-span-3 border-solid border-2 border-black">
-          <div class="max-w-lg bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
+      {/* START OF CARDS */}
+      <div className="grid py-10 px-20 md:grid-cols-6 gap-8 border-solid ">
+        <div className="md:flex md:flex-col md:justify-center md:gap-2 border-solid hidden ">
+          <div
+            style={{
+              width: "0.25rem",
+              height: "1.13rem",
+              borderRadius: "0l125rem",
+              backgroundColor: "rgb(10 135 118)",
+            }}
+          ></div>
+          <div
+            style={{
+              width: "0.25rem",
+              height: "1.13rem",
+              borderRadius: "0l125rem",
+              backgroundColor: "rgb(10 135 118)",
+            }}
+          ></div>
+          <div
+            style={{
+              width: "0.25rem",
+              height: "1.13rem",
+              borderRadius: "0l125rem",
+              backgroundColor: "rgb(10 135 118)",
+            }}
+          ></div>
+          <div
+            style={{
+              width: "0.25rem",
+              height: "1.13rem",
+              borderRadius: "0l125rem",
+              backgroundColor: "rgb(10 135 118)",
+            }}
+          ></div>
+        </div>
+        <div className="md:col-span-3 border-solid ">
+          <div className="max-w-lg bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <div className="overflow-hidden w-full max-h-52">
               <img
-                class="rounded-t-lg"
-                src="/docs/images/blog/image-1.jpg"
+                className="rounded-t-lg"
+                src={require("../Designs/TwitterImageLogo.png")}
                 alt=""
               />
-            </a>
-            <div class="p-5">
-              <a href="#">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            </div>
+            <div className="p-5">
+              <div>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                   Noteworthy technology acquisitions 2021
                 </h5>
-              </a>
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              </div>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                 Here are the biggest enterprise technology acquisitions of 2021
-                so far, in reverse chronological order.
+                so far, in reverse chronological order. Here are the biggest
+                enterprise technology acquisitions of 2021 so far, in reverse
+                chronological order.Here are the biggest enterprise technology
+                acquisitions of 2021 so far, in reverse chronological order.
               </p>
-              <a
-                href="#"
-                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Read more
-                <svg
-                  aria-hidden="true"
-                  class="w-4 h-4 ml-2 -mr-1"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </a>
             </div>
           </div>
         </div>
-        <div className="col-span-2 border-solid border-2 border-black">hi</div>
+        <div className="flex flex-row items-center justify-center gap-2 md:col-span-2 border-solid ">
+          <img
+            className="w-10 h-10 rounded-full"
+            src={require("../Designs/ProfilePic.jpg")}
+            alt=""
+          />
+          <div className="font-medium dark:text-white">
+            <div>CURATED FOR</div>
+            <div>Jese Leos</div>
+            <div className="text-sm text-gray-500 dark:text-gray-400">
+              Joined in August 2014
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
